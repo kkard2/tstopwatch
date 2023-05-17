21feeffeae3bf8df1d0eb3db15195368cb7829f0
kkard2 <kkard2.acct@gmail.com>
2023-05-17 18:25:08 +0200

    feat: current time

--- src/draw.rs
+++ src/draw.rs
@@ -17,14 +17,22 @@ const HIGHLIGHT_COLORS: Colors = Colors {
     foreground: Some(Color::Yellow),
     background: Some(Color::Black),
 };
+const TIMESTAMP_COLORS: Colors = Colors {
+    foreground: Some(Color::Cyan),
+    background: Some(Color::Black),
+};
 
 pub fn draw(stdout: &mut std::io::Stdout, state: &AppState) -> crossterm::Result<()> {
+    let size = crossterm::terminal::size()?;
     stdout
         .queue(Clear(ClearType::All))?
         .queue(Hide)?
         .queue(MoveTo(0, 0))?;
 
     for (i, stack) in state.stacks().iter().enumerate() {
+        if i > (size.1 - 2) as usize {
+            break;
+        }
         stdout.queue(SetColors(if state.current_stack_index() == i {
             HIGHLIGHT_COLORS
         } else {
@@ -34,6 +42,11 @@ pub fn draw(stdout: &mut std::io::Stdout, state: &AppState) -> crossterm::Result
         draw_stopwatch(stdout, stack.current())?;
     }
 
+    stdout
+        .queue(SetColors(TIMESTAMP_COLORS))?
+        .queue(MoveTo(0, size.1 - 1))?;
+
+    draw_timestamp(stdout, &chrono::Local::now())?;
     stdout.queue(SetColors(NORMAL_COLORS))?;
     stdout.flush()
 }
@@ -50,6 +63,17 @@ fn draw_stopwatch(
     let elapsed_str = format!("{:02}:{:02}:{:02}.{:03}", hours, minutes, seconds, millis);
 
     stdout.queue(Print(elapsed_str))?;
+
+    Ok(())
+}
+
+fn draw_timestamp(
+    stdout: &mut std::io::Stdout,
+    timestamp: &chrono::DateTime<chrono::Local>,
+) -> crossterm::Result<()> {
+    let timestamp_str = format!("{}", timestamp.format("%Y-%m-%d %H:%M:%S"));
+
+    stdout.queue(Print(timestamp_str))?;
 
     Ok(())
 }
